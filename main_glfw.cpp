05d73ffa2267c0fff0b9330bf1470c76c0326ee4
Mark Reid <mindmark@gmail.com>
2022-11-10 23:06:00 -0800

    Add HIDPI scaling
    Signed-off-by: Mark Reid <mindmark@gmail.com>

--- main_glfw.cpp
+++ main_glfw.cpp
@@ -80,6 +80,8 @@ int main(int argc, char** argv)
     //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
     io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;           // Enable Docking
     io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;         // Enable Multi-Viewport / Platform Windows
+    io.ConfigFlags |= ImGuiConfigFlags_DpiEnableScaleFonts;
+    io.ConfigFlags |= ImGuiConfigFlags_DpiEnableScaleViewports;
     // io.ConfigFlags |= ImGuiConfigFlags_EnablePowerSavingMode;
     io.ConfigViewportsNoAutoMerge = true;
     //io.ConfigViewportsNoTaskBarIcon = true;
